f0e5f4730b896e25736a77277ca3a5aa532a7dc6
sssanx <130704199+sssanx@users.noreply.github.com>
2026-09-22 23:04:11 -0600

    Refactor integration configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
- https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),   //  Habilita React (necesario para Material UI)
-    tailwind() //  Activa Tailwind CSS
+    react(),
+    tailwind()
   ],
-  output: 'server', //  Genera salida del lado del servidor
+  output: 'server',
 });
-
