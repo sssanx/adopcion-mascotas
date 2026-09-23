c713b87fdc5a7f25fda762dffd93415153cb7c99
sssanx <130704199+sssanx@users.noreply.github.com>
2026-09-22 22:58:28 -0600

    Update astro.config.mjs for React and Tailwind
    Uncommented and updated configuration for React and Tailwind integration, changed output type to 'server'.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,14 @@
 // @ts-check
-//import { defineConfig } from 'astro/config';
-//import react from '@astrojs/react';
-//import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-//export default defineConfig({
-  //integrations: [
-    //react(),   // 🔹 Habilita React (necesario para Material UI)
-    //tailwind() // 🔹 Activa Tailwind CSS
-  //],
-  //output: 'server', // 🔹 Genera salida del lado del servidor
-//});
-
-// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+ https://astro.build/config
 export default defineConfig({
-  site: 'https://sssanx.github.io',
-  base: '/adopcion-mascotas',
   integrations: [
-    react(),
-    tailwind()
+    react(),   //  Habilita React (necesario para Material UI)
+    tailwind() //  Activa Tailwind CSS
   ],
-  output: 'static', // 🔹 GitHub Pages solo sirve archivos estáticos
+  output: 'server', //  Genera salida del lado del servidor
 });
+
