f0f87396327fd814dd696b9445d89bf59d20a60a
sssanx <130704199+sssanx@users.noreply.github.com>
2026-09-22 22:53:00 -0600

    Configure site and base for GitHub Pages
    Updated site and base properties for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,29 @@
 // @ts-check
+//import { defineConfig } from 'astro/config';
+//import react from '@astrojs/react';
+//import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+//export default defineConfig({
+  //integrations: [
+    //react(),   // 🔹 Habilita React (necesario para Material UI)
+    //tailwind() // 🔹 Activa Tailwind CSS
+  //],
+  //output: 'server', // 🔹 Genera salida del lado del servidor
+//});
+
+// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sssanx.github.io',
+  base: '/adopcion-mascotas',
   integrations: [
-    react(),   // 🔹 Habilita React (necesario para Material UI)
-    tailwind() // 🔹 Activa Tailwind CSS
+    react(),
+    tailwind()
   ],
-  output: 'server', // 🔹 Genera salida del lado del servidor
+  output: 'static', // 🔹 GitHub Pages solo sirve archivos estáticos
 });
